2952564e5ccf4b12ec47d135ae1358f600dc78bf
devops-akbar <akbarm0105@gmail.com>
2026-04-22 15:10:42 +0530

    test

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://13.213.52.216:84"
+const API_BASE_URL = "alb-backend-1753536639.ap-southeast-1.elb.amazonaws.com"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
